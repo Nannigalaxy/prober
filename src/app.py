cee7cbb1c9c65ab3edb78a2345cc8bf86c5c0653
Nandan Manjunatha <Nannigalaxy@gmail.com>
2024-02-19 19:07:19 +0000

    domain certificate details

--- src/app.py
+++ src/app.py
@@ -1,24 +1,16 @@
-from os import environ 
-import re
+from os import environ
 
 import requests
 from flask import Flask, render_template, request
 from flask_moment import Moment
 
-from utils import read_yaml
+from utils import (get_domain_certificate, get_root_domain, re_url_checker,
+                   read_yaml)
 
 YAML_FILE_URL = environ.get('FEED_YAML') # Path to source yaml file
 
 app = Flask(__name__)
 moment = Moment(app)
-
-
-def probe_api(url: str):
-    try:
-        response = requests.get(url)
-    except Exception as e:
-        return str(e)
-    return str(response.status_code)
 
 
 def get_feed_data(filename):
@@ -31,25 +23,56 @@ def get_feed_data(filename):
     )
 
 
+feed_data = get_feed_data(YAML_FILE_URL)
+
+
+def probe_api(url: str):
+    try:
+        response = requests.get(url)
+    except Exception as e:
+        return str(e)
+    return str(response.status_code)
+
+
+def get_domain_metadata(domain_list: str):
+    domain_data = {}
+    for domain in domain_list:
+        domain_data[domain] = get_domain_certificate(domain)
+    return domain_data
+
+
+def get_metadata():
+    target_info = feed_data["target_info"]
+    domain_list = []
+    for target in target_info:
+        for entity in target_info[target]:
+            for value in entity.values():
+                if re_url_checker(value):
+                    domain_list.append(get_root_domain(value))
+
+    domain_list = list(set(domain_list))
+    domain_data = get_domain_metadata(domain_list)
+    return domain_data
+
+
 @app.route("/probe_status", methods=["GET"])
 def probe_status():
-    query_url = str(request.args.get('url'))
+    query_url = str(request.args.get("url"))
     request_status = probe_api(query_url)
     return request_status
 
 
 @app.route("/", methods=["GET"])
 def index():
-    feed_data = get_feed_data(YAML_FILE_URL)
     map_target = feed_data["map_target"]
     status_check = feed_data["status_check"]
     target_info = feed_data["target_info"]
-    pattern = "^http.*|^www\..*"
-    re_func = lambda x: re.match(pattern, x)
+    domain_data = get_metadata()
     return render_template(
         "status.html",
         column_model=map_target,
         status_check=status_check,
         target_data=target_info,
-        link_checker=re_func
+        link_checker=re_url_checker,
+        domain_data=domain_data,
     )
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,39 @@
-import yaml
+import re
+import ssl
+from datetime import datetime
+
+import OpenSSL
 import requests
+import yaml
+from tld import get_tld
+
 
 def read_yaml(url_filename: str) -> dict:
     file = requests.get(url_filename)
     prime_service = yaml.safe_load(file.text)
     return prime_service
+
+
+def re_url_checker(url: str) -> bool:
+    pattern = "^http.*|^www\..*"
+    re_func = lambda x: re.match(pattern, x)
+    return bool(re_func(url))
+
+
+def get_root_domain(url: str) -> str:
+    res = get_tld(url, as_object=True)
+    return res.fld
+
+
+def get_domain_certificate(url: str):
+    cert = ssl.get_server_certificate((url, 443))
+    x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
+    b_date = str(x509.get_notAfter(), encoding="utf-8")[:8]
+    exp_date = datetime.strptime(b_date, "%Y%m%d").date()
+    cert_remain_days = (exp_date - datetime.now().date()).days
+    cert_exp_date = exp_date.strftime("%d %B %Y")
+    cert_issuer = x509.get_issuer().O
+    cert_data = dict(
+        expire_date=cert_exp_date, days_left=cert_remain_days, issuer=cert_issuer
+    )
+    return cert_data
